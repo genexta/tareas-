45d9cb859425298a41e2ec697ecad3b3b5df6210
genexta <43440842+genexta@users.noreply.github.com>
2019-08-26 21:08:43 -0400

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+const = user =[
+	{id: 11, nombre: 'adam ', edad:23 , grupo:'editor'  },
+	{id: 47, nombre: 'jhon ', edad:28 , grupo:'admin'  },
+	{id: 85, nombre: 'william ', edad:34 , grupo:'editor'},
+	{id: 97, nombre: 'oliver', edad:28 , grupo:'admin '}
+];
+const user = arrary.filter( v => v === ohn );
+
+
+cnost euros =[29.76, 41.85, 46.5];
+const euros = array.filter (euros  => (euros[0] + euros[1] + euros[2])/3 );
---
+++ ejercicios.js
@@ -0,0 +1,48 @@
+ejercicios
+
+1.- let people =["steve" , "pete" , "mike","nick"]
+
+let people =["steve" ,"nick"];
+let people2 = [ "pete" , "mike"];
+people = [...peolpe2,...people];
+console.log(people);
+
+2.- let user = {name:"steve", age: 22}
+let details = {place: "romania", hobby:"reading" };
+unir los 2 objetos en 1
+
+let user1 = [...user ,...details];
+
+3.-user ={name: "pedro", lastname:"poveda",details:{phone:4553120,dir:"directoin 123"}}
+obtener la variable phone y dir 
+
+user ={name: "pedro", lastname:"poveda",details:{phone:4553120,dir:"directoin 123"}}
+console.log[details{dir}];
+
+iventiva2019@gmail.com
+
+const = divbythree = array.filter( v => v % 3 === 0 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
